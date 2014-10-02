f7b62d41cdc8cfb2063257c3f39764d314e95e03
Jad Khoriaty <jadk92@gmail.com>
2014-10-02 18:41:38 -0400

    Creation of Emergency Team Dispatcher solution

---
+++ Emergency Team Dispatcher/Emergency Team Dispatcher/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Emergency_Team_Dispatcher
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
