8258b87c1131190a5fd10f9ea9c8f983e6133ad3
Andrew J <aiwu>
2014-10-21 21:43:51 -0400

    private to public for TeamMember class

--- Emergency Team Dispatcher/Emergency Team Dispatcher/TeamMember.cs
+++ Emergency Team Dispatcher/Emergency Team Dispatcher/TeamMember.cs
@@ -5,21 +5,29 @@ using System.Text;
 
 namespace Emergency_Team_Dispatcher
 {
-    class TeamMember
+    public class TeamMember
     {
-        String name;
+        public String name;
         public int trainingLevel; //0: First Aid, 1: First Responder, 2: Medicine
-        public String departure;
+        public DateTime departure;
 
-        public TeamMember(String name, int training, String dep)
+        public TeamMember(String name, int training)
         {
             this.name = name;
             this.trainingLevel = training;
-            this.departure = dep;
-            //departure.AddHours(23-departure.Hour);
-            //departure.AddMinutes(59 - departure.Minute);
-            //departure.AddSeconds(59 - departure.Second);
+            departure = DateTime.Now;
+            departure.AddHours(23-departure.Hour);
+            departure.AddMinutes(59 - departure.Minute);
+            departure.AddSeconds(59 - departure.Second);
 
         }
+        public TeamMember(String name, int training, DateTime dep)
+        {
+            this.name = name;
+            if(training == 0 || training ==1)
+                this.trainingLevel = training;
+            if (DateTime.Now.CompareTo(dep) < 0)
+                this.departure = dep;
+        }   
     }
 }
