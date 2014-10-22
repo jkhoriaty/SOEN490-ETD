b99321d66b38033cb84d3dc1ee3cd9448d9256a4
Andrew J <aiwu>
2014-10-21 21:39:19 -0400

    unit tests
    Unit Test Project ( add corresponding files to test)

---
+++ Emergency Team Dispatcher/EmergencyTeamDispatcherTest/TeamMemberTest.cs
@@ -0,0 +1,22 @@
+﻿using Emergency_Team_Dispatcher;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmergencyTeamDispatcherTest
+{
+    [TestClass]
+    public class TeamMemberTest
+    {
+        [TestMethod]
+        public void TeamMemberCreation()
+        {
+            String name = "Test";
+            int trainLevel = 9000;
+            TeamMember a = new TeamMember(name, trainLevel);
+            Assert.AreEqual(a.name, "Test");
+            Assert.AreEqual(a.trainingLevel, 9000);
+
+        }
+    }
+
+}
