c52bd8f2187287a1074f4fa1dbb75d8962f6ec33
Andrew J <aiwu>
2014-10-30 20:06:18 -0400

    unit tests
    updated some tests

--- Emergency Team Dispatcher/EmergencyTeamDispatcherTest/TeamTest.cs
+++ Emergency Team Dispatcher/EmergencyTeamDispatcherTest/TeamTest.cs
@@ -9,8 +9,31 @@ namespace EmergencyTeamDispatcherTest
     public class TeamTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TeamCreation()
         {
+            String name = "Test";
+            Team T = new Team(name);
+            Assert.AreEqual(T.getName(), "Test");
+            T.setName("Bill");
+            Assert.AreEqual(T.getName(), "Bill");
+        }
+
+        [TestMethod]
+        public void defaultTeamCreation()
+        {
+            Team T = new Team();
+            Assert.AreEqual(T.getName(), "Alpha");
+        }
+
+        [TestMethod]
+        public void addMemberTest()
+        {
+            Team T = new Team();
+            TeamMember testMember = new TeamMember("John",9999,"1500");
+            T.addMember(testMember);
+            Assert.AreEqual(T.getMember(0), null);
+            Assert.AreEqual(T.getMember(9999), testMember);
         }
     }
+
 }
