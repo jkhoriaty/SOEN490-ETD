af59c06833816c25e752622c2ae7715b66bad9d8
Andrew J <aiwu>
2014-11-16 22:39:38 -0500

    added Unit Test for ETD

---
+++ ETD/ETD_UnitTest/TimerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ETD;
+
+namespace ETD_UnitTest
+{
+    [TestClass]
+    public class TimerTest
+    {
+        [TestMethod]
+        public void StartTimerTest()
+        {
+            Timer.StartTimer();
+            Assert.AreEqual(Timer.currentIntervention, 1);
+            Assert.AreEqual(Timer.currentTeam, 0);
+        }
+    }
+
+
+}
