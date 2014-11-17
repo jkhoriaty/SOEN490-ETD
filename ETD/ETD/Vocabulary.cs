1dcc7cce1cb715817ee4061bf790c4d1aeb37882
Walter Chacon <walter.chacon@gmail.com>
2014-11-17 15:30:09 -0500

    Feature Language Selector - Development: - Create base model classes[ Word, Vocabulary] for the language selection feature

---
+++ ETD/ETD/Vocabulary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace ETD
+{
+    // Class containing the set of words in a vocabulary associated with a unique identifier.
+    class Vocabulary
+    {
+        private Dictionary<String, Word> vocabulary; // <identifier, word>
+
+        public Vocabulary()
+        {
+            this.vocabulary = new Dictionary<String, Word>();
+            loadVocabulary();
+        }
+
+        public void loadVocabulary()
+        {
+            using (XmlReader reader = XmlReader.Create("..\\..\\Vocabulary.xml"))
+            {
+                String name = "", fr = "", en = "";
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        switch (reader.Name)
+                        {
+                            case "control": name = reader.GetAttribute("name");
+                                break;
+                            case "fr": fr = reader.ReadElementContentAsString().Trim();
+                                break;
+                            case "en": en = reader.ReadElementContentAsString().Trim();
+                                vocabulary.Add(name, new Word(fr, en));
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ETD/ETD/Word.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ETD
+{
+    // Class containing the multilingual equivalent of a word. One string variable per language.
+    // Can be expanded by adding more.
+    class Word
+    {
+        private String fr { get; set; } // French version of a word.
+        private String en { get; set; } // English version of a word.
+
+        public Word(String f, String e)
+        {
+            this.fr = f;
+            this.en = e;
+        }
+    }
+}
