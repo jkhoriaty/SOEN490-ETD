bd075024ad98ea2b40f0308bd8faecf84d1cbb95
Andrew <andrew.jias@gmail.com>
2015-01-13 22:29:39 -0800

    added some tests, removed some un-used variables

--- ETD/ETD/ViewsPresenters/InterventionsSection/InterventionForm/InterventionFormPage.xaml.cs
+++ ETD/ETD/ViewsPresenters/InterventionsSection/InterventionForm/InterventionFormPage.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using ETD.ViewsPresenters.InterventionsSection;
+//using ETD.ViewsPresenters.InterventionsSection;
 using ETD.ViewsPresenters.InterventionsSection.InterventionForm.TimersInterventionForm;
 using ETD.ViewsPresenters.InterventionsSection.InterventionForm.DetailsInterventionForm;
 using ETD.ViewsPresenters.InterventionsSection.InterventionForm.ResourcesInterventionForm;
