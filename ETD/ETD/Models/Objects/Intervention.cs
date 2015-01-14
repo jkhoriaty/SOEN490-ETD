e129a14507e282e5c504ca4497e4ed987d476355
sahiln25 <sahiln25@gmail.com>
2015-01-13 22:12:44 -0500

    ABC text changes
    Replaced RSP with chest pain and replaced absent with no pulse.

--- ETD/ETD/Models/Objects/Intervention.cs
+++ ETD/ETD/Models/Objects/Intervention.cs
@@ -9,7 +9,7 @@ namespace ETD.Models.Objects
 	public enum Consciousness { alert, verbal, painful, unconscious, notSet };
 	public enum Airways { clear, partiallyObstructed, completelyObstructed, notSet };
 	public enum Breathing { normal, difficulty, absent, notSet };
-	public enum Circulation { normal, rsp, hemorrhage, absent, notSet };
+	public enum Circulation { normal, chestPain, hemorrhage, noPulse, notSet };
 	public enum Conclusions { returnToSite, returnToHome, referredToDoctor, equipmentDistribution, hospital, patientNotFound, noInterventions, other, notSet };
 
     public class Intervention
