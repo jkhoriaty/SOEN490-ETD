a2ef908874a10d9f4c778e9266a8d321261157f3
Jad Khoriaty <jadk92@gmail.com>
2015-02-11 00:53:01 -0500

    Creation of Observer pattern needed classes that will be used for the upcoming over-haul

---
+++ ETD/ETD/Models/Objects/Observable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETD.Models.Objects
+{
+	interface Observable
+	{
+		private List<Observer> observers;
+		public void registerObserver(Observer observer);
+		private void notifyAll();
+	}
+}
