464d442d24cb595bf68e0a458cd9463e29325bbe
sahiln25 <sahiln25@gmail.com>
2015-02-25 17:00:57 -0500

    Re-added classes that got deleted in merge
    Fixed the errors from the code.

---
+++ ETD/ETD/Models/Objects/InterventionAdditionalInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETD.Models.Objects
+{
+    public class InterventionAdditionalInfo
+    {
+        private String information;
+        private DateTime timestamp;
+
+        public InterventionAdditionalInfo(String information, DateTime timestamp)
+        {
+            this.information = information;
+            this.timestamp = timestamp;
+        }
+    }
+}
