0e283f6943495df23760968af5f6465446c7832a
Andrew <andrew.jias@gmail.com>
2015-03-01 23:14:15 -0500

    fixing revert

---
+++ ETD/ETD_UnitTest/IntegrationTest/TeamUITest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ETD.Models;
+using ETD.ViewsPresenters.MapSection;
+using ETD.ViewsPresenters.MapSection.PinManagement;
+using ETD.ViewsPresenters;
+using ETD.Models.Objects;
+namespace ETD_UnitTest
+{
+    [TestClass]
+    public class ResourceUITest
+    {
+        [TestMethod]
+      
+		public void CreateTeamPinTest()
+		{
+			MainWindow window = new MainWindow();
+			MapSectionPage mapSectionPage = new MapSectionPage(window);
+			Team a = new Team("A");
+			mapSectionPage.Update();
+			
+			bool check = false;
+			foreach(Pin pin in Pin.getPinList())
+			{
+				if(pin.getRelatedObject() == a)
+				{
+					check = true;
+				}
+			}
+			Assert.IsTrue(check);
+		
+           
+        }        
+    }
+}
