6d9bff1cf6ac3a4c0d4440d5c86bf66b1005e153
Quynh92 <anigamjo@hotmail.com>
2015-03-20 16:07:00 -0400

    bug fix and ui improvements

--- ETD/ETD/Services/TechnicalServices.cs
+++ ETD/ETD/Services/TechnicalServices.cs
@@ -72,12 +72,12 @@ namespace ETD.Services
         //Sets the image path for each map modification 
         private static Dictionary<MapMods, String> MapModPath = new Dictionary<MapMods, String>
         {
-            {MapMods.camp, @"\Icons\camp.png"},
+            {MapMods.camp, @"\Icons\camp2.png"},
             {MapMods.circle, @"\Icons\lineCanvas.png"},
             {MapMods.line, @"\Icons\lineCanvas.png"},
             {MapMods.rectangle, @"\Icons\lineCanvas.png"},
             {MapMods.square, @"\Icons\lineCanvas.png"},
-            {MapMods.stairs, @"\Icons\stairs.png"},
+            {MapMods.stairs, @"\Icons\stairs2.png"},
             {MapMods.ramp, @"\Icons\Ramp.png"}
         };
 
