0a0b50db7ecb6fb35e8e676e2b90c5aa87261258
Andrew <andrew.jias@gmail.com>
2015-04-02 02:55:58 -0400

    Mapper for volunteer statistic added

---
+++ ETD/ETD_Statistic/Model/VolunteerStatistic.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using ETD_Statistic.Model;
+using ETD.Services.Database;
+using System.Collections;
+
+namespace ETD_Statistic.Model
+{
+    class VolunteerStatistic
+    {
+        String volunteerName;
+        DateTime startTime;
+        DateTime endTime;
+        TimeSpan timeDiff;
+        String operationID;
+        public VolunteerStatistic(String name, DateTime start, DateTime end, String opID)
+        {
+            volunteerName = name;
+            startTime = start;
+            endTime = end;
+            timeDiff = getDateDifference(start,end);
+            operationID = opID;
+        }
+
+        public void setName(String name)
+        {
+            volunteerName = name;
+        }
+
+        public String getName()
+        {
+            return volunteerName;
+        }
+
+        public void setStart(DateTime start)
+        {
+            startTime = start;
+        }
+
+        public DateTime getStart()
+        {
+            return startTime;
+        }
+
+        public void setEnd(DateTime end)
+        {
+            endTime = end;
+        }
+
+        public DateTime getEnd()
+        {
+            return endTime;
+        }
+
+        public TimeSpan getTimeDiff()
+        {
+            return timeDiff;
+        }
+
+        public String getOperationID()
+        {
+            return operationID;
+        }
+
+        public TimeSpan getDateDifference(DateTime start, DateTime end)
+        {
+            TimeSpan timeDifference = end.Subtract(start);
+            return timeDifference;
+        }
+
+    }
+}
---
+++ ETD/ETD_Statistic/Model/VolunteerStatisticMapper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETD.Services.Database;
+using System.Data.SQLite;
+
+
+namespace ETD_Statistic.Model
+{
+    class VolunteerStatisticMapper
+    {
+        private List<VolunteerStatistic> volunteerStatisticList = new List<VolunteerStatistic>();
+        String dbQuery = "SELECT Volunteers.Name as Name, Operation_ID, Joined, Departure FROM Team_Members JOIN Teams ON Teams.Team_ID = Team_Members.Team_ID JOIN Volunteers ON Team_Members.Volunteer_ID = Volunteers.Volunteer_ID WHERE Operation_ID IN " + Statistic.getOperationID();
+
+        public VolunteerStatisticMapper()
+        {
+            SQLiteDataReader reader = StaticDBConnection.QueryDatabase(dbQuery);
+            while (reader.Read())
+            {
+                String volunteer = reader["Name"].ToString();
+                DateTime start = Convert.ToDateTime(reader["Joined"].ToString());
+                DateTime end = Convert.ToDateTime(reader["Departure"].ToString());
+                String operationID = reader["Operation_ID"].ToString();
+
+                //Creating volunteerStatistic
+                VolunteerStatistic vs = new VolunteerStatistic(volunteer, start, end, operationID);
+                volunteerStatisticList.Add(vs);
+            }
+            StaticDBConnection.CloseConnection();
+        }
+
+        public List<VolunteerStatistic> getList()
+        {
+            return volunteerStatisticList;
+        }
+
+        public void ClearList()
+        {
+            volunteerStatisticList.Clear();
+        }
+
+
+
+    }
+}
